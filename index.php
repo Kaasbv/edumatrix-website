ac97a63133acaaf17f46035e5ff2e414d477e09b
steef96 <steef96.sb@gmail.com>
2020-12-01 23:31:01 +0100

    nav+jumbo

--- index.php
+++ index.php
@@ -3,16 +3,43 @@
     <head>
         <meta charset="utf-8">
         <title>Patat</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="style.css">    
     </head>
 <body>
-    <h2>Is het patat of friet?</h2>
-    <?php
-       $opties = ["patat"];
-       echo $opties[array_rand($opties)];
-    ?>
 
+    <nav>
+        <div id= "navbar">
+        <img id="menu-logo" src="./Images/logo.png" alt="Logo"></img>
+        <a href="PLACEHOLDER1">PLACEHOLDER1</a>
+        <a href="PLACEHOLDER2">PLACEHOLDER2</a>
+        <a href="PLACEHOLDER3">PLACEHOLDER3</a>
+        <a href="PLACEHOLDER4">PLACEHOLDER4</a>
+        <a href="PLACEHOLDER5">PLACEHOLDER5</a>
+        <button id= "offerte" class="btn btn-outline-info" onclick="document.location= 'PLACEHOLDER-OFFERTE'"> Offerte</button>
+        </div>
+    </nav>
+
+    <div id="welkom">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1> TITLE HERE </h1>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare lacus massa, ut tincidunt turpis dapibus et. Duis rhoncus ante eu urna sodales semper. Nunc sodales efficitur dolor, eget commodo eros finibus vitae. Integer vel facilisis velit. Quisque mauris mauris, iaculis ac tincidunt sit amet, semper a tortor. Mauris nec imperdiet ante, porttitor fermentum sapien. Duis sollicitudin a turpis at placerat. In id erat vestibulum, ornare eros id, porttitor mi. Proin nibh ante, consectetur quis magna ac, pharetra finibus lacus. Nam venenatis urna vel purus molestie venenatis. Proin non lorem vitae massa sagittis congue at id ante. Vestibulum non turpis placerat, dictum ligula in, lacinia neque. Vestibulum odio augue, vulputate ut vulputate vitae, porta non enim. Praesent vehicula vehicula nibh a porta. Nullam eget lacinia tortor, vel porta mi. Nam sed aliquet eros.</p>
+                    <button class="btn btn-outline-info" onclick="document.location= 'PLACEHOLDER'"> placeholder</button>
+                    <button class="btn btn-outline-info" onclick="document.location= 'PLACEHOLDER'"> placeholder</button>
+                </div>
+                <div class="col">
+                    <img id="blankpc" src= ./Images/blankpc.png alt="blankpc"></img>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
 </body>
 </html>

57e7d8ec73877a7f5844235c5aa16a2cbd62f500
burak <b.bayraktar@student.avans.nl>
2021-03-27 09:45:16 +0100

    logo, pc en tekst update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="nl">
     <head>
         <meta charset="utf-8">
-        <title>Patat</title>
+        <title>Edumatrix</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
@@ -26,8 +26,8 @@
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <h1> TITLE HERE </h1>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare lacus massa, ut tincidunt turpis dapibus et. Duis rhoncus ante eu urna sodales semper. Nunc sodales efficitur dolor, eget commodo eros finibus vitae. Integer vel facilisis velit. Quisque mauris mauris, iaculis ac tincidunt sit amet, semper a tortor. Mauris nec imperdiet ante, porttitor fermentum sapien. Duis sollicitudin a turpis at placerat. In id erat vestibulum, ornare eros id, porttitor mi. Proin nibh ante, consectetur quis magna ac, pharetra finibus lacus. Nam venenatis urna vel purus molestie venenatis. Proin non lorem vitae massa sagittis congue at id ante. Vestibulum non turpis placerat, dictum ligula in, lacinia neque. Vestibulum odio augue, vulputate ut vulputate vitae, porta non enim. Praesent vehicula vehicula nibh a porta. Nullam eget lacinia tortor, vel porta mi. Nam sed aliquet eros.</p>
+                    <h1> EDUMATRIX </h1>
+                    <p> Edumatrix is een nieuwe kijk op het leerlingvolgsysteem. Met zijn moderne en stijlvolle design is het overzichtelijk. Aangepast naar behoeften van scholen en leerlingen. Een alles omvattende systeem!</p>
                     <button class="btn btn-outline-info" onclick="document.location= 'PLACEHOLDER'"> placeholder</button>
                     <button class="btn btn-outline-info" onclick="document.location= 'PLACEHOLDER'"> placeholder</button>
                 </div>
